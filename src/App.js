c0fe6cd23a95859f0a9b85fafe64cd45419708b8
lwx13960404305 <lwx1654878819@163.com>
2019-11-07 14:52:28 +0800

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>你好</h1>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  state = {
+    users: []
+  }
+
+ componentWillMount(){
+
+axios.get('http://mock-api.com/jzpddjzX.mock/test')
+.then((res)=>{ 
+  let {data} = res
+console.log(data)
+
+this.setState({
+  users: data
+})
+
+})
+     
+    
+  }
+   
+  render() {
+    let {users} =this.state
+   
+    return (
+      
+      <div>
+        <div className="a"></div>
+        {
+          users.map((item) => (
+            <div className="ab"> <li >{item.name}</li>
+            <li >{item.price}</li>
+            <li >{item.created_at}</li></div>
+          
+          ))
+        }
+      </div>
+    )
+
+  }
 }
 
 export default App;
